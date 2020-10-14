8907911250fa37f35602bd3b46ced9c0b646fcfd
Lorilyn Jordan Miller <jordan@archemedx.com>
2020-10-14 00:11:35 -0400

    refactor is better

--- sosc/src/sosc/csv_data.clj
+++ sosc/src/sosc/csv_data.clj
@@ -2,78 +2,103 @@
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
-(def data
-  (with-open [reader (io/reader "/Users/ljmiller/Desktop/Projects/personal/science-of-second-chances/sosc/resources/data.csv")]
-    (doall
-     (csv/read-csv reader))))
-
 (defn str->int [s]
   (try (Integer/parseInt s)
        (catch Exception _ nil)))
 
-(defn columnizer [column-index]
-  (let [column
-        (mapv #(nth % column-index) data)
-
-        attribute
-        (keyword (first column))
-
-        values
-        (mapv str->int (rest column))]
-    (assoc {} attribute values)))
-
-(def xf (map columnizer))
-(def column-by-attribute
-    (transduce xf conj (range 10))) ; this is a list of maps, each map has a key that corresponds to an attribute and a value that is a list of the values
-
-
-
-
-
+(def data
+  (with-open [reader (io/reader "/sosc/resources/data.csv")]
+    (doall
+     (csv/read-csv reader))))
 
 (defn csv-data->maps [data]
   (map zipmap
-       (->> (first data)  ;; First row is the header
-            (map keyword) ;; Drop if you want string keys instead
+       (->> (first data)
+            (map keyword)
             repeat)
        (map (fn [m] (mapv str->int m)) (rest data))))
 
-(def all-hired-candidates (csv-data->maps data)) ;this is a list of maps, each map is one hired person
-(count all-hired-candidates)
+(def all-hired-candidates
+  (csv-data->maps data))
 
-(defn valid-candidates [all-hired-candidates] ;28 persons get passed in
-  (letfn [(fields-present? [row] (some nil? (vals row))) ;check for any missing fields
+
+(defn valid-candidates [all-hired-candidates]
+  (letfn [(fields-present? [row] (some nil? (vals row))) ;; Check for any missing fields
           (job-position [{:keys [position]}] (or (= position 1)
-                                                 (= position 2)))] ;only counts sales and customer service positions
+                                                 (= position 2)))]
     (->> all-hired-candidates
-         (remove fields-present?)
-         (filter job-position))))
+         (remove fields-present?)  ;; Remove rows with missing data
+         (filter job-position))))  ;; Only count sales and customer service positions
+
 
 (def valid-hired-candidates (valid-candidates all-hired-candidates))  ; all valid hired candidates
-(count valid-hired-candidates) ;16
 
-(defn criminal-history [all-hired-candidates-clean]
+
+(comment
+  (count all-hired-candidates)
+  (count valid-hired-candidates)
+
+  (defn columnizer [column-index]
+    (let [column
+          (mapv #(nth % column-index) data)
+
+          attribute
+          (keyword (first column))
+
+          values
+          (mapv str->int (rest column))]
+      (assoc {} attribute values)))
+
+  (def xf (map columnizer))
+  (def column-by-attribute
+    (transduce xf conj (range 10))) ; this is a list of maps, each map has a key that corresponds to an attribute and a value that is a list of the values
+)
+
+
+(defn criminal-history? [valid-hired-candidates]
   (letfn [(criminal-history? [{:keys [criminal-history]}]
             (= criminal-history 1))]
-    (->> all-hired-candidates-clean
-         (filter criminal-history?))))
+    {:second-chance-candidates
+     (->> valid-hired-candidates
+          (filter criminal-history?))
 
-(def hired-candidates-with-CH (criminal-history valid-hired-candidates))
-(count hired-candidates-with-CH) ; 2
+     :no-criminal-history
+     (->> valid-hired-candidates
+          (remove criminal-history?))}))
+
+(comment
+  (def criminal-histories (criminal-history? valid-hired-candidates))
+  => {:second-chance-candidates
+      ({:ID 1, :longest-job 2, :school 1, :fewest-short-jobs -2, :voluntary-t 0, :LOE 1236, :terminated 0, :misconduct 0, :position 1, :criminal-history 1, :involuntary-t 0} {:ID 4, :longest-job 3, :school 0, :fewest-short-jobs -5, :voluntary-t 0, :LOE 1000, :terminated 1, :misconduct 0, :position 1, :criminal-history 1, :involuntary-t 1} {:ID 14, :longest-job 1, :school 1, :fewest-short-jobs -3, :voluntary-t 0, :LOE 1036, :terminated 0, :misconduct 0, :position 2, :criminal-history 1, :involuntary-t 0} {:ID 19, :longest-job 2, :school 1, :fewest-short-jobs -4, :voluntary-t 0, :LOE 1236, :terminated 0, :misconduct 0, :position 2, :criminal-history 1, :involuntary-t 0}
+       {:ID 24365, :longest-job 2, :school 1, :fewest-short-jobs 1, :voluntary-t 1, :LOE 653, :terminated 1, :misconduct 1, :position 1, :criminal-history 1, :involuntary-t 0}
+       {:ID 26242, :longest-job 1, :school 1, :fewest-short-jobs -5, :voluntary-t 1, :LOE 735, :terminated 1, :misconduct 0, :position 2, :criminal-history 1, :involuntary-t 0})
+
+      :no-criminal-history
+      ({:ID 13, :longest-job 1, :school 0, :fewest-short-jobs -2, :voluntary-t 1, :LOE 89, :terminated 1, :misconduct 0, :position 2, :criminal-history 0, :involuntary-t 0}
+       {:ID 16, :longest-job 3, :school 1, :fewest-short-jobs -4, :voluntary-t 0, :LOE 1936, :terminated 0, :misconduct 0, :position 2, :criminal-history 0, :involuntary-t 0}
+       {:ID 4325, :longest-job 4, :school 1, :fewest-short-jobs -2, :voluntary-t 0, :LOE 234, :terminated 1, :misconduct 0, :position 2, :criminal-history 0, :involuntary-t 1} {:ID 6345, :longest-job 4, :school 1, :fewest-short-jobs -3, :voluntary-t 0, :LOE 543, :terminated 1, :misconduct 1, :position 1, :criminal-history 0, :involuntary-t 1} {:ID 526, :longest-job 6, :school 1, :fewest-short-jobs -1, :voluntary-t 0, :LOE 253, :terminated 1, :misconduct 0, :position 2, :criminal-history 0, :involuntary-t 1})})
 
 (defn average
   [numbers]
-  (if (empty? numbers)
-    0
+  (if (empty? numbers) 0
     (float (/ (reduce + numbers) (count numbers)))))
 
 (defn length-of-employment [candidate-pool]
   (map (fn [{:keys [LOE]}] LOE) candidate-pool))
 
-(def average-LOE-of-hired-candidates [:all (average (length-of-employment valid-hired-candidates))])
-(def average-LOE-of-CH-candidates [:CH-only (average (length-of-employment hired-candidates-with-CH))])
+(defn compare-LOE [valid-hired-candidates]
+  (let [criminal-histories
+        (criminal-history? valid-hired-candidates)
 
-(max-key second average-LOE-of-hired-candidates average-LOE-of-CH-candidates)
+       no-criminal-history-LOE
+       [:all (average (length-of-employment (:no-criminal-history criminal-histories)))]
+
+       second-chance-candidate-LOE
+       [:second-chance (average (length-of-employment (:second-chance-candidates criminal-histories)))]]
+    (max-key second no-criminal-history-LOE second-chance-candidate-LOE)))
+
+(comment
+   (compare-LOE valid-hired-candidates))
 
 (defn quit-vs-fired [candidate-pool]
   (letfn [(terminated? [{:keys [terminated]}]
@@ -108,5 +133,7 @@
             (filter misconduct?)
             count)})))
 
-(quit-vs-fired hired-candidates-with-CH)
-(quit-vs-fired valid-hired-candidates)
+
+(comment
+  (quit-vs-fired hired-candidates-with-CH)
+  (quit-vs-fired valid-hired-candidates))
