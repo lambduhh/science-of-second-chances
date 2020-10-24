92ca8f2a38ff906a5d1a003e027427947b43ce37
Lorilyn Jordan Miller <jordan@archemedx.com>
2020-10-23 20:40:17 -0400

    add presentation code

--- sosc/src/sosc/core.clj
+++ sosc/src/sosc/core.clj
@@ -18,25 +18,7 @@
             repeat)
        (map (fn [m] (mapv str->int m)) (rest data))))
 
-(def all-hired-candidates
-  (csv-data->maps data))
-
-(defn columnizer [column-index] ; this was just some fun I was having with sorting the data by columns
-  (let [column
-        (mapv #(nth % column-index) data)
-
-        attribute
-        (keyword (first column))
-
-        values
-        (mapv str->int (rest column))]
-    (assoc {} attribute values)))
-
-(def xf (map columnizer))
-(def column-by-attribute
-  (transduce xf              conj             []  (range 10))
-  ; transduce transforming-fns reducing  []  coll
-  )
+(def all-hired-candidates (csv-data->maps data))
 
 (defn valid-candidates [all-hired-candidates]
   (letfn [(fields-present? [row] (some nil? (vals row)))
@@ -44,7 +26,28 @@
                                                  (= position 2)))]
     (->> all-hired-candidates
          (remove fields-present?)
-         (filter job-position))))
+         (filter job-position)))) ; data scrubber
+
+(def valid-hired-candidates (valid-candidates all-hired-candidates))
+
+;;;;;;;;;;;;;;
+;; BAD CODE ;; -> why?
+;;;;;;;;;;;;;;
+
+(defn criminal-history? [valid-hired-candidates]
+  (letfn [(criminal-history? [{:keys [criminal-history]}]
+            (= criminal-history 1))]
+    {:second-chance-candidates
+     (->> valid-hired-candidates
+          (filter criminal-history?))
+
+     :no-criminal-history
+     (->> valid-hired-candidates
+          (remove criminal-history?))}))
+
+;;;;;;;;;;;;;;
+;; GOOD CODE ;;
+;;;;;;;;;;;;;;
 
 (defn separate-gen-pop [valid-hired-candidates]
   (reduce (fn [acc {:keys [criminal-history] :as person}]
@@ -54,22 +57,12 @@
           {:second-chance-candidates []
            :no-criminal-history []}
           valid-hired-candidates))
-(comment
-  (def separated-by-ch (separate-gen-pop valid-hired-candidates))
 
-  => {:second-chance-candidates
-      ({:ID 1, :longest-job 2, :school 1, :fewest-short-jobs -2, :voluntary-t 0, :LOE 1236, :terminated 0, :misconduct 0, :position 1, :criminal-history 1, :involuntary-t 0}
-       {:ID 4, :longest-job 3, :school 0, :fewest-short-jobs -5, :voluntary-t 0, :LOE 1000, :terminated 1, :misconduct 0, :position 1, :criminal-history 1, :involuntary-t 1} {:ID 14, :longest-job 1, :school 1, :fewest-short-jobs -3, :voluntary-t 0, :LOE 1036, :terminated 0, :misconduct 0, :position 2, :criminal-history 1, :involuntary-t 0} {:ID 19, :longest-job 2, :school 1, :fewest-short-jobs -4, :voluntary-t 0, :LOE 1236, :terminated 0, :misconduct 0, :position 2, :criminal-history 1, :involuntary-t 0} {:ID 24365, :longest-job 2, :school 1, :fewest-short-jobs 1, :voluntary-t 1, :LOE 653, :terminated 1, :misconduct 1, :position 1, :criminal-history 1, :involuntary-t 0} {:ID 26242, :longest-job 1, :school 1, :fewest-short-jobs -5, :voluntary-t 1, :LOE 735, :terminated 1, :misconduct 0, :position 2, :criminal-history 1, :involuntary-t 0} {:ID 6536, :longest-job 2, :school 1, :fewest-short-jobs -3, :voluntary-t 1, :LOE 632, :terminated 1, :misconduct 0, :position 1, :criminal-history 1, :involuntary-t 0}
-       {:ID 2535, :longest-job 2, :school 1, :fewest-short-jobs -3, :voluntary-t 1, :LOE 723, :terminated 1, :misconduct 0, :position 2, :criminal-history 1, :involuntary-t 0}
-       {:ID 6436, :longest-job 1, :school 1, :fewest-short-jobs -1, :voluntary-t 1, :LOE 733, :terminated 1, :misconduct 0, :position 1, :criminal-history 1, :involuntary-t 0})
-      , :no-criminal-history
-      ({:ID 13, :longest-job 1, :school 0, :fewest-short-jobs -2, :voluntary-t 1, :LOE 89, :terminated 1, :misconduct 0, :position 2, :criminal-history 0, :involuntary-t 0}
-       {:ID 16, :longest-job 3, :school 1, :fewest-short-jobs -4, :voluntary-t 0, :LOE 1936, :terminated 0, :misconduct 0, :position 2, :criminal-history 0, :involuntary-t 0}
-       {:ID 18, :longest-job 4, :school 1, :fewest-short-jobs -3, :voluntary-t 0, :LOE 1036, :terminated 0, :misconduct 0, :position 2, :criminal-history 0, :involuntary-t 0}
-       {:ID 24, :longest-job 4, :school 1, :fewest-short-jobs -5, :voluntary-t 0, :LOE 1234, :terminated 1, :misconduct 0, :position 1, :criminal-history 0, :involuntary-t 1}
-       {:ID 4325, :longest-job 4, :school 1, :fewest-short-jobs -2, :voluntary-t 0, :LOE 234, :terminated 1, :misconduct 0, :position 2, :criminal-history 0, :involuntary-t 1}
-       {:ID 6345, :longest-job 4, :school 1, :fewest-short-jobs -3, :voluntary-t 0, :LOE 543, :terminated 1, :misconduct 1, :position 1, :criminal-history 0, :involuntary-t 1}
-       {:ID 526, :longest-job 6, :school 1, :fewest-short-jobs -1, :voluntary-t 0, :LOE 253, :terminated 1, :misconduct 0, :position 2, :criminal-history 0, :involuntary-t 1})})
+
+;;;;;;;;;;;;;;
+;; helpers ;;
+;;;;;;;;;;;;;;
+
 
 (defn average
   [numbers]
@@ -79,20 +72,19 @@
 (defn length-of-employment [candidate-pool]
   (map :LOE candidate-pool))
 
-; so we have these two helpers abstracted out.. could be beneficial to use in other places
-; this function takes "valid-hired-candidates" data instead of (separate-gen-pop valid-hired-candidates) by design..
-;  -> all the fns in the app are meant to take the same input and do the data transforamtion internally
-;   -> if each fn does its own transformation, less fn dependent logic means that it's likely a bug in one function can detrimentally effect other parts/functions
-; easy to follow, very declarive/expressive?
+
+;;;;;;;;;;;;;;
+;; MEDIO-CODE ;; -> why?
+;;;;;;;;;;;;;;
 
 
-(defn compare-loe-alpha [valid-hired-candidates]
+(defn compare-LOE-alpha [valid-hired-candidates]
   (let [average-LOE
         (->> (length-of-employment valid-hired-candidates)
              (average))
 
         criminal-histories
-        (criminal-history valid-hired-candidates)
+        (separate-gen-pop valid-hired-candidates)
 
         no-criminal-history-LOE
         (->> (:no-criminal-history criminal-histories)
@@ -113,20 +105,6 @@
      :difference-in-tenure      (if (< second-chance-LOE no-criminal-history-LOE)
                                   (str (- no-criminal-history-LOE second-chance-LOE) " days")
                                   (str (- second-chance-LOE no-criminal-history-LOE) " days"))}))
-(comment
-  (compare-loe-alpha valid-hired-candidates))
-; change those caps to lowercase (somebody once told me that "dont use uppercase, everyone will hate you)
-; move the length-of-employment function into a letfn (we only use it here)
-;  -> average is a very abstract helper that may have use in other places.. I would typically move to a 'utils' file
-; use comp instead of thread-last
-
-; (how many lines did I actually save?)
-; (what are the trade-off of using comp?)
-; ->  similar to partial (oh its fancy, make you feel powerful :) )
-;   -> so a traditionalist functional POV may encourage? but vs idiomatic clojure discourages
-; (did it make it more OR less readable?)
-; (benefit of starting to us comp is training for transducers)
-
 
 (defn compare-loe-second [valid-hired-candidates]
   (letfn [(length-of-employment [candidate-pool]
@@ -136,7 +114,7 @@
                (average))
 
           separated-gen-pop
-          (criminal-history valid-hired-candidates)
+          (separate-gen-pop valid-hired-candidates)
 
           ave-loe
           (comp average length-of-employment)
@@ -147,9 +125,9 @@
           second-chance-loe
           (ave-loe (:second-chance-candidates separated-gen-pop))]
       {:average-loe               gen-pop-average-loe
-       :second-chance-loe         second-chance-LOE
+       :second-chance-loe         second-chance-loe
        :no-criminal-history-loe   no-criminal-history-loe
-       :longest-tenure-on-average (if (< second-chance-LOE no-criminal-history-loe)
+       :longest-tenure-on-average (if (< second-chance-loe no-criminal-history-loe)
                                     "no criminal history"
                                     "second-chance-candidates")
 
@@ -157,12 +135,10 @@
                                     (str (- no-criminal-history-loe second-chance-loe) " days")
                                     (str (- second-chance-loe no-criminal-history-loe) " days"))})))
 
-; still some things wrong with this...
-; what if we chose to pass in the value of (separate-gen-pop valid-hired-candidates)?
-; -> still need to get 'all-candidates' to return average-loe
-; another concern is that since we are passing in the map that is the result of a former fn
-; -> opens up to concerns about "passing down bugs"
-; heading in the right direction !
+
+; This time passing in the value of (separate-gen-pop valid-hired-candidates)
+;  instead of valid hired candidates
+
 
 (defn compare-loe-third [{:keys [second-chance-candidates
                                  no-criminal-history]}]
@@ -183,15 +159,18 @@
       {:average-loe               gen-pop-average-loe
        :second-chance-loe         second-chance-loe
        :no-criminal-history-loe   no-criminal-history-loe
-       :longest-tenure-on-average (if (< second-chance-LOE no-criminal-history-loe)
+       :longest-tenure-on-average (if (< second-chance-loe no-criminal-history-loe)
                                     "no criminal history"
                                     "second-chance-candidates")
 
-       :difference-in-tenure      (if (< second-chance-loe no-criminal-history-loe)
-                                    (str (- no-criminal-history-loe second-chance-loe) " days")
-                                    (str (- second-chance-loe no-criminal-history-loe) " days"))})))
+       :difference-in-tenure (if (< second-chance-loe no-criminal-history-loe)
+                               (str (- no-criminal-history-loe second-chance-loe) " days")
+                               (str (- second-chance-loe no-criminal-history-loe) " days"))})))
 
-; dont want to mix in rendering vs data.. separate view logic out
+;;;;;;;;;;;;;;
+;; GOOD CODE ;;
+;;;;;;;;;;;;;;
+
 
 (defn compare-loe-fourth [{:keys [second-chance-candidates
                                   no-criminal-history]}]
@@ -213,10 +192,6 @@
        :second-chance-loe         second-chance-loe
        :no-criminal-history-loe   no-criminal-history-loe})))
 
-(comment
-   (def separated-by-ch (separate-gen-pop valid-hired-candidates))
-  (compare-loe-fourth separated-by-ch))
-
 (defn render-compare-loe [{:keys [average-loe
                                   second-chance-loe
                                   no-criminal-history-loe] :as data}]
@@ -230,22 +205,10 @@
           (str (- no-criminal-history-loe second-chance-loe) " days")
           (str (- second-chance-loe no-criminal-history-loe) " days"))]
 
-   (str "Those that " longest-tenure-on-average " had the longest tenure on average."
-        " The difference is " diff-in-tenure)))
+    (str "Those that " longest-tenure-on-average " had the longest tenure on average."
+         " The difference is " diff-in-tenure)))
 
-(comment
-  (render-compare-loe (compare-loe-fourth separated-by-ch))
-
-  {:average-loe                  average-loe
-   :loe-second-chance            second-chance-loe
-   :loe-no-criminal-history      no-criminal-history-loe
-   :second-chance-longer-tenure? (if (< second-chance-loe no-criminal-history-loe)
-                                   "no criminal history"
-                                   "second-chance-candidates")
-
-   :difference-in-tenure         (Math/abs (< second-chance-loe no-criminal-history-loe))}
-  (def old-compare-loe (compare-loe valid-hired-candidates)))
-
+;TO-DO refactor
 (defn quit-vs-fired [candidate-pool]
   (letfn [(terminated? [{:keys [terminated]}]
             (= terminated 1))
@@ -278,3 +241,18 @@
        (->> terminated-employees
             (filter misconduct?)
             count)})))
+
+(defn columnizer [column-index] ; this was just some fun I was having building transducers and sorting the data by columns
+  (let [column
+        (mapv #(nth % column-index) data)
+
+        attribute
+        (keyword (first column))
+
+        values
+        (mapv str->int (rest column))]
+    (assoc {} attribute values)))
+
+(def xf (map columnizer))
+(def column-by-attribute
+  (transduce xf conj [] (range 10)))
